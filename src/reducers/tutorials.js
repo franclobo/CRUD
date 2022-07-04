cb007c0d5a25c5b977d163c0520c28befa58785f
franclobo <fjbl2788@gmail.com>
2022-07-03 20:03:30 -0500

    CReate redux reducers.

---
+++ src/reducers/tutorials.js
@@ -0,0 +1,30 @@
+import {
+  CREATE_TURORIAL,
+  RETRIEVE_TUTORIALS,
+  UPDATE_TUTORIAL,
+  DELETE_TUTORIAL,
+  DELETE_ALL_TUTORIALS,
+} from "../actions/types";
+
+const initialState = [];
+function tutorialsReducer(tutorials = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case CREATE_TURORIAL:
+      return [...tutorials, payload];
+    case RETRIEVE_TUTORIALS:
+      return payload;
+    case UPDATE_TUTORIAL:
+      return tutorials.map((tutorial) =>
+        tutorial._id === payload._id ? {...tutorial, ...payload} : tutorial
+      );
+    case DELETE_TUTORIAL:
+      return tutorials.filter(({ id }) => id !== payload.id);
+    case DELETE_ALL_TUTORIALS:
+      return [];
+    default:
+      return tutorials;
+  }
+};
+
+export default tutorialsReducer;
